063729a692a095c5f96d99d3b94b3ce05ffb3ef9
v1zun0 <pijarzuliano@gmail.com>
2024-04-01 14:58:46 +0700

    firstcommit

---
+++ Web Portofolio/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  var nav = document.querySelector("nav ul");
+  nav.classList.toggle("active");
+  var burger = document.querySelector(".burger");
+  burger.classList.toggle("active");
+}
